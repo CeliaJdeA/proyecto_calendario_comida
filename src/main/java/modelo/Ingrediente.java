f1678a7b116a4cf60c48a3049c40b7e5365367ee
Celia <micorreo@gmail.com>
2024-05-06 18:09:30 +0200

    Estructura del proyecto

---
+++ src/main/java/modelo/Ingrediente.java
@@ -0,0 +1,61 @@
+package modelo;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "calendario_comidas")
+public class Ingrediente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_ingrediente")
+	private int idIngrediente;
+	private String nombre;
+	@Column(name = "fk_lista_compra")
+	private int lista;
+	
+	
+	public int getIdIngrediente() {
+		return idIngrediente;
+	}
+	public void setIdIngrediente(int idIngrediente) {
+		this.idIngrediente = idIngrediente;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getLista() {
+		return lista;
+	}
+	public void setLista(int lista) {
+		this.lista = lista;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(idIngrediente);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ingrediente other = (Ingrediente) obj;
+		return idIngrediente == other.idIngrediente;
+	}
+	
+	
+}
---
+++ src/main/java/negocio/CalendarioImpl.java
@@ -0,0 +1,16 @@
+package negocio;
+
+import java.util.List;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Array;
+
+import modelo.Ingrediente;
+
+public class CalendarioImpl implements Calendario{
+
+	@Override
+	public List<Ingrediente> lista() {
+		return ;
+	}
+
+}
